ba911128322299c980b20e1a868a7287033d3653
apalent <145934550+apalent@users.noreply.github.com>
2023-09-25 23:34:45 -0400

    Update IdCardForm.js

--- src/IdCardForm.js
+++ src/IdCardForm.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import './IdCardForm.css'; // Create this CSS file for styling
 import { saveAs } from 'file-saver';
-import templateImage from './template.jpeg'; // Import the template image
+import idcardtemplate from './idcardtemplate.jpeg'; // Import the template image
 
 const IdCardForm = () => {
   const [name, setName] = useState('');
@@ -18,69 +18,104 @@ const IdCardForm = () => {
     const file = e.target.files[0];
     setImage(file);
   };
+  
+const generateIdCard = () => {
+    // Create a canvas to generate the ID card
+    const canvas = document.createElement('canvas');
+    const ctx = canvas.getContext('2d');
+  
+    // Set canvas dimensions for a standard ID card size
+    canvas.width = 425;
+    canvas.height = 625;
+  
+    // Draw the template image as a background
+    const templateImg = new Image();
+    templateImg.src = idcardtemplate;
+    templateImg.onload = () => {
+      ctx.drawImage(templateImg, 0, 0, canvas.width, canvas.height);
+  
+      // Draw the name on the template image (independent of profile)
+      ctx.font = 'bold 23px monospace';
+      ctx.fillStyle = 'black';
+      const text = `${name}`;
+      const textWidth = ctx.measureText(text).width; // Get the width of the text
+      
+      // Calculate the X and Y coordinates to center the text
+      const centerX = (canvas.width - textWidth) / 2;
+  
+      ctx.fillText(text, centerX, canvas.height / 1.55); // Adjust the position for the name
+        // Draw other details like Date of Birth, Bank, Branch, Mobile, and Blood Group
+       // Set a monospaced font
+ctx.font = 'bold 19px monospace'; // Replace 'monospace' with the actual monospaced font you prefer
+ctx.fillStyle = 'black';
 
-  const generateIdCard = () => {
-    if (!name || !dob || !address || !bank || !branch || !mobile || !bloodGroup || !image) {
-      alert('Please fill in all fields and upload an image.');
-      return;
-    }
+// Define the text labels and values
+const labels = ['Bank        :', 'Branch      : ', 'Mobile      :', 'Blood Group :'];
+const values = [ bank, branch, mobile, bloodGroup];
+
+// Calculate the Y position for text
+const startY = 440; // Adjust the starting Y position as needed
+
+// Loop through the labels and values
+for (let i = 0; i < labels.length; i++) {
+  const label = labels[i];
+  const value = values[i];
+
+  // Calculate the X position for text
+  const labelX = 50;
+  const valueX = 200; // Adjust this value to create space between the label and value
+
+  // Calculate the Y position for this label
+  const textY = startY + i * 30; // Adjust the vertical spacing as needed
+
+  // Draw the label and value
+  ctx.fillText(`${label}`, labelX, textY);
+  ctx.fillText(` ${value}`, valueX, textY);
+}
+
+
+
+   
+     // Create a circular clip path with a 2 cm radius
+      const circleRadius = 4 * 28.35; // 2 cm to pixels (assuming 1cm = 28.35 pixels)
+      const circleX = 213; // X-coordinate of the circle center
+      const circleY = canvas.height / 2 - canvas.height * 0.1; // Y-coordinate of the circle center
   
-    // Create a data URL for the uploaded image
-    const reader = new FileReader();
-    reader.onload = () => {
-      const imgDataUrl = reader.result;
+      ctx.beginPath();
+      ctx.arc(circleX, circleY, circleRadius, 0, 2 * Math.PI);
+      ctx.closePath();
+      ctx.clip();
   
-      // Create a canvas to generate the ID card
-      const canvas = document.createElement('canvas');
-      const ctx = canvas.getContext('2d');
-      canvas.width = 400; // Adjust the canvas size as needed
-      canvas.height = 250;
+      // Draw the profile picture inside the circular clip path
+      const img = new Image();
+      img.src = URL.createObjectURL(image);
+      img.onload = () => {
+        const profileX = circleX - circleRadius; // Center horizontally
+        const profileY = circleY - circleRadius; // Center vertically
+        const profileSize = circleRadius * 2; // Diameter of the circle
   
-      // Draw the template image as a background
-      const templateImg = new Image();
-      templateImg.src = templateImage;
-      templateImg.onload = () => {
-        ctx.drawImage(templateImg, 0, 0, canvas.width, canvas.height);
+        ctx.drawImage(img, profileX, profileY, profileSize, profileSize);
   
-        // Add name and date of birth to the ID card
-        ctx.font = 'bold 20px Arial';
-        ctx.fillStyle = 'black';
-        ctx.fillText(`Name: ${name}`, 20, 60); // Adjust the position for the name
-        ctx.fillText(`Date of Birth: ${dob}`, 20, 90); // Adjust the position for the date of birth
   
-        // Draw the uploaded image on the canvas (left side)
-        const img = new Image();
-        img.onload = () => {
-          ctx.drawImage(img, 14, 14 , 140, 140); // Adjust position and size as needed (left side)
-          
-          // Add Bank, Branch, Mobile, and Blood Group below the photo
-          ctx.font = '16px Arial';
-          ctx.fillText(`Bank: ${bank}`, 20, 270); // Adjust position for Bank
-          ctx.fillText(`Branch: ${branch}`, 20, 290); // Adjust position for Branch
-          ctx.fillText(`Mobile: ${mobile}`, 20, 310); // Adjust position for Mobile
-          ctx.fillText(`Blood Group: ${bloodGroup}`, 20, 330); // Adjust position for Blood Group
+        // Convert the canvas to a data URL
+        const idCardDataURL = canvas.toDataURL('image/png');
   
-          // Convert the canvas to a data URL
-          const idCardDataURL = canvas.toDataURL('image/png');
-  
-          // Set the generated ID card image in state
-          setIdCardImage(idCardDataURL);
-        };
-        img.src = imgDataUrl;
+        // Set the generated ID card image in state
+        setIdCardImage(idCardDataURL);
       };
     };
-    reader.readAsDataURL(image);
   };
+  
   
 
   const saveIdCardImage = () => {
-    if (!idCardImage || !name || !dob || !bank || !branch || !mobile || !bloodGroup) {
-      alert('Please generate the ID card image first.');
-      return;
-    }
+    // if (!idCardImage || !name || !dob || !bank || !branch || !mobile || !bloodGroup) {
+    //   alert('Please generate the ID card image first.');
+    //   return;
+    // }
 
     // Generate a file name based on name and date of birth
-    const fileName = `${name.replace(/\s/g, '_')}_${dob}.png`;
+    const fileName = `${name.replace(/\s/g, '_')}_${mobile}.jpeg`;
 
     // Fetch the generated ID card image and save it with the calculated file name
     fetch(idCardImage)
@@ -88,7 +123,7 @@ const IdCardForm = () => {
       .then((blob) => {
         saveAs(blob, fileName);
       })
-      .then(() => saveToDB());
+    //   .then(() => saveToDB());
   };
 
   const saveToDB = () => {
@@ -100,48 +135,49 @@ const IdCardForm = () => {
   return (
     <div className="id-card-form">
       <h1>ID Card Generator</h1>
-      <div className="form-group">
-        <label htmlFor="name">Name:</label>
-        <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="dob">Date of Birth:</label>
-        <input type="text" id="dob" value={dob} onChange={(e) => setDob(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="address">Address:</label>
-        <input type="text" id="address" value={address} onChange={(e) => setAddress(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="bank">Bank:</label>
-        <input type="text" id="bank" value={bank} onChange={(e) => setBank(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="branch">Branch:</label>
-        <input type="text" id="branch" value={branch} onChange={(e) => setBranch(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="mobile">Mobile:</label>
-        <input type="text" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="bloodGroup">Blood Group:</label>
-        <input type="text" id="bloodGroup" value={bloodGroup} onChange={(e) => setBloodGroup(e.target.value)} />
-      </div>
-      <div className="form-group">
-        <label htmlFor="image">Upload Image:</label>
-        <input type="file" accept="image/*" id="image" onChange={handleImageUpload} />
-      </div>
-      <button onClick={generateIdCard}>Preview</button>
-
-      {idCardImage && (
-        <div className="id-card-container">
-          <img src={idCardImage} alt="ID Card" />
+      <div className="input-section">
+        <div className="form-group">
+          <label htmlFor="name">Name:</label>
+          <input type="text" id="name" value={name} onChange={(e) => setName(e.target.value)} />
         </div>
-      )}
-      {idCardImage && (
-        <button onClick={saveIdCardImage}>Save ID Card Image</button>
-      )}
+        <div className="form-group">
+          <label htmlFor="dob">Date of Birth:</label>
+          <input type="text" id="dob" value={dob} onChange={(e) => setDob(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="address">Address:</label>
+          <input type="text" id="address" value={address} onChange={(e) => setAddress(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="bank">Bank:</label>
+          <input type="text" id="bank" value={bank} onChange={(e) => setBank(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="branch">Branch:</label>
+          <input type="text" id="branch" value={branch} onChange={(e) => setBranch(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="mobile">Mobile:</label>
+          <input type="text" id="mobile" value={mobile} onChange={(e) => setMobile(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="bloodGroup">Blood Group:</label>
+          <input type="text" id="bloodGroup" value={bloodGroup} onChange={(e) => setBloodGroup(e.target.value)} />
+        </div>
+        <div className="form-group">
+          <label htmlFor="image">Upload Image:</label>
+          <input type="file" accept="image/*" id="image" onChange={handleImageUpload} />
+        </div>
+        <button onClick={generateIdCard}>Preview</button>
+      </div>
+      <div className="preview-section">
+        {idCardImage && (
+          <div className="id-card-container">
+            <img src={idCardImage} alt="ID Card" />
+            <button onClick={saveIdCardImage}>Save ID Card Image</button>
+          </div>
+        )}
+      </div>
     </div>
   );
 };
